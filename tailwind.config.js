bd38d52df2f46ad8f26cddacd06c6944e51f4373
urix <extremtmnt@gmail.com>
2022-04-16 15:52:22 +0300

    feat: apply @tailwindcss/forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
@@ -21,5 +23,5 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
