3da9143cf973f5b1b543525431773369574a0976
urix <extremtmnt@gmail.com>
2022-04-16 16:07:40 +0300

    chore: setup lint-staged via husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  '*.js': ['eslint --max-warnings=0 --no-ignore'],
+  '*.{vue,ts,tsx}': [
+    'eslint --max-warnings=0 --no-ignore',
+    () => 'vue-tsc --noEmit --skipLibCheck',
+  ],
+}
