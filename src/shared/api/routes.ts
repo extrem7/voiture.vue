b40f3abeba6b592b1d2903044468fd5da18e2eff
MadMax <sharkkizayk@gmail.com>
2022-04-25 14:46:01 +0300

    feat: add choose marks witout image =(

--- src/shared/api/routes.ts
+++ src/shared/api/routes.ts
@@ -2,4 +2,6 @@ export const api = {
   categories: () => '/categories',
   models: (id: number, idMarks: number) =>
     `/categories/${id}/marks/${idMarks}/models`,
+  marks: (id: number) => `/categories/${id}/marks`,
+  body: (id: number) => `/categories/${id}/bodystyles`,
 }
---
+++ src/shared/ui/VChooseMarks/VChooseMarks.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryFn } from '@storybook/vue3'
+import type { PickProps } from '~/shared/lib/storybook'
+import { VChooseMarks } from '.'
+
+type Props = PickProps<typeof VChooseMarks>
+
+const meta: Meta = {
+  title: 'UI/VChooseMarks',
+  component: VChooseMarks,
+}
+
+export default meta
+
+export const Default: StoryFn<Props> = (props) => (
+  <VChooseMarks {...props}>Menu Link</VChooseMarks>
+)
+
+Default.storyName = 'VChooseMarks'
