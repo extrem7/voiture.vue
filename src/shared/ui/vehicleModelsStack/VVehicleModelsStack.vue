ed13e7d7b76996b49a037650c3a25220068dee64
MadMax <sharkkizayk@gmail.com>
2022-04-26 17:54:12 +0300

    feat: create `vehicle-models-stack` and new api-query

---
+++ src/shared/ui/vehicleModelsStack/VVehicleModelsStack.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useModelGroups } from '~/shared/api'
+
+const isFull = ref(false)
+const id = ref(1)
+const idMarks = ref(5)
+const { groups } = useModelGroups(id, idMarks)
+const fullGroups = computed(() => groups.value.flat())
+const someGroups = computed(() => groups.value.flat().slice(0, 10))
+</script>
+
+<template>
+  <div class="w-[300px]">
+    <!-- добавити динамічно 'назва марки' -->
+    <h2 class="mb-2 text-xl">Виберіть модель Мерседес</h2>
+    <!-- добавити динамічно -->
+    <div class="grid grid-cols-2 gap-2 mb-2">
+      <a
+        v-for="{ name, value } in isFull ? fullGroups : someGroups"
+        :key="value"
+        class="text-[#256799] border-[1px] border-gray hover:border-[#256799] duration-150 cursor-pointer"
+      >
+        <p
+          class="overflow-x-hidden mx-auto w-10/12 text-center whitespace-nowrap hover:border-[#256799] duration-100"
+        >
+          {{ name }}
+        </p>
+      </a>
+    </div>
+    <span
+      v-if="fullGroups.length > 10"
+      :class="[
+        isFull ? 'before:rotate-180 before:-top-[2px] before:left-[113%]' : '',
+      ]"
+      class="relative before:absolute before:top-[5px] before:left-[105%] text-[#256799] before:border-8 before:border-x-blue/0 before:border-b-blue/0 border-dotted hover:border-solid duration-200 cursor-pointer"
+      @click="isFull = !isFull"
+    >
+      <!-- добавити динамічно 'назва марки' -->
+      {{ isFull ? 'Сховати' : `ще ${fullGroups.length - 10} моделей mercedes` }}
+      <!-- добавити динамічно -->
+    </span>
+  </div>
+</template>
