65786db0b44b71d123f493aaef499791b82c6932
manura18 <mary.kondreskul@gmail.com>
2022-05-02 19:05:38 +0300

    feat(ui): add VPaginationButton

---
+++ src/shared/ui/VPaginationButton/VPaginationButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+  isActive: Boolean,
+  isDisabled: Boolean,
+})
+</script>
+
+<template>
+  <a
+    :class="{
+      'text-blue border-blue': isActive,
+      'hover:text-blue hover:border-blue': !isDisabled,
+    }"
+    class="py-[4px] px-[8px] min-w-[40px] text-lg text-center text-dark rounded-lg border border-gray"
+  >
+    <slot />
+  </a>
+</template>
---
+++ src/shared/ui/VPaginationButton/index.ts
@@ -0,0 +1,1 @@
+export { default as VPaginationButton } from './VPaginationButton.vue'
