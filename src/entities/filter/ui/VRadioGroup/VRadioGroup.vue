fed4bb117f9d7b04c92fed3f153030a43e432f71
AirFast <airfast.88@gmail.com>
2022-05-19 22:32:24 +0300

    refactor: formatting and removing redundant code

--- src/entities/filter/ui/VRadioGroup/VRadioGroup.vue
+++ src/entities/filter/ui/VRadioGroup/VRadioGroup.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 const carConditions = [
   { value: 'all', name: 'Всі' },
   { value: 'used', name: 'Вживані' },
   { value: 'new', name: 'Нові' },
   { value: 'import', name: 'Під пригон' },
-];
+]
 
-const condition = ref('all');
+const condition = ref('all')
 </script>
 
 <template>
@@ -16,15 +16,15 @@ const condition = ref('all');
     <span v-for="{ name, value } in carConditions" :key="value" class="group">
       <input
         :id="value"
-        :value="value"
         v-model="condition"
+        :value="value"
         type="radio"
         name="carConditions"
         class="peer hidden"
       />
       <label
         :for="value"
-        class="peer-checked:bg-white peer-checked:text-dark peer-checked:before:content-['✔'] before:content-none before:mr-2 before:text-red text-sm bg-red text-white p-2 border border-red cursor-pointer duration-100 group-first:rounded-l-md group-last:rounded-r-md"
+        class="p-2 before:mr-2 text-sm before:text-red text-white peer-checked:text-dark before:content-none peer-checked:before:content-['✔'] bg-red peer-checked:bg-white group-last:rounded-r-md group-first:rounded-l-md border border-red duration-100 cursor-pointer"
       >
         {{ name }}
       </label>
